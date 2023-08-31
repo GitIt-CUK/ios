4197424568fda27365159aa470cb8c089caed1c4
gadisom <jeongwon.kim64@gmail.com>
2023-08-31 13:35:14 +0900

    feat : 로고 디자인 및 로그인 디자인

---
+++ Let's_Git_It/Font/Font.swift
@@ -0,0 +1,12 @@
+//
+//  Font.swift
+//  Let's_Git_It
+//
+//  Created by 김정원 on 2023/08/31.
+//
+
+import Foundation
+import UIKit
+
+let font = UIFont(name: "NanumSquareRoundB", size: 15.0)
+
---
+++ Let's_Git_It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Let's_Git_It
+//
+//  Created by 김정원 on 2023/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
